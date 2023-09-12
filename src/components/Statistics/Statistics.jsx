11834fb0af61668d83e216764c4a19e3dd69d9a7
Matviienko Ruslana <brauniesrus@gmail.com>
2023-09-12 12:39:42 +0300

    react hw1

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -6,14 +6,15 @@ export const Statistics = ({ title, stats }) => {
 
   return (
     <section className={css.statistics}>
-      <h2 className={css.title}>{title}</h2>
+      {title && <h2 className={css.title}>{title}</h2>}
 
       <ul className={css.stat_list}>
         {stats.map(stat => (
           <li
             key={stat.id}
             className={css.item}
-            style={{ backgroundColor: getRandomHexColor() }}>
+            style={{ backgroundColor: getRandomHexColor() }}
+          >
             <span className={css.label}>{stat.label}</span>
             <span className={css.percentage}>{stat.percentage}%</span>
           </li>
